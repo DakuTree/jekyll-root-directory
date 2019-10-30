2da0b900516b1b88ce54ab341948b5a79bdbcf22
DakuTree <dakutree@codeanimu.net>
2019-10-30 08:57:22 +0000

    init `bundle gem jekyll-root-directory --test=rspec --mit --no-coc`

---
+++ lib/jekyll/root/directory.rb
@@ -0,0 +1,10 @@
+require "jekyll/root/directory/version"
+
+module Jekyll
+  module Root
+    module Directory
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/root/directory/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Root
+    module Directory
+      VERSION = "0.1.0"
+    end
+  end
+end
